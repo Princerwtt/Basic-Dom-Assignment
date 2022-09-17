dc9569585f7e59c12689d79865ef277b694829d5
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2022-09-17 20:16:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-vdyqxx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-vdyqxx)
---
+++ script.js
@@ -0,0 +1,26 @@
+
+	  	function prince(){
+	  		document.getElementById("root").style.color="red";
+	  		
+	  	}
+
+	  	function prince1(){
+  		var divData=document.getElementsByClassName("blue-white");
+  		divData[0].style.color="white";
+        var divdata=document.getElementsByClassName("blue-white");
+  		divdata[0].style.backgroundColor="blue";
+  		}
+
+  		function prince2(){
+  		var divdata=document.getElementsByClassName("blue-white");
+  		divdata[1].style.color="white";
+        var divdata=document.getElementsByClassName("blue-white");
+  		divdata[1].style.backgroundColor="blue";
+     
+  		}
+
+  		function prince3(){
+  		var pData= document.getElementsByTagName("p");
+  		pData[1].style.fontSize="50px";
+  		}
+  
